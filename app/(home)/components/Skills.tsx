7db5cf0791f49d82fba68e69346433973e4b5347
Satyabrata Saha <108542741+satyabrata-saha@users.noreply.github.com>
2025-03-21 23:51:47 +0530

    " update Skills component with additional skills and improve layout"

--- app/(home)/components/Skills.tsx
+++ app/(home)/components/Skills.tsx
@@ -1,36 +1,33 @@
 "use client";
 
-import React from 'react'
-import Title from './Title'
-import { HoverEffect } from '@/components/ui/card-hover-effect'
-import { SiNextdotjs, SiReact, SiTailwindcss } from 'react-icons/si'
+import React from "react";
+import Title from "./Title";
+import { HoverEffect } from "@/components/ui/card-hover-effect";
+import {
+  SiCss3,
+  SiGit,
+  SiHtml5,
+  SiJavascript,
+  SiNextdotjs,
+  SiReact,
+  SiSupabase,
+  SiTailwindcss,
+  SiWordpress,
+} from "react-icons/si";
 
 export default function Skills() {
-
   const skills = [
     {
-      text: "React",
-      Icon: SiReact,
+      text: "HTML",
+      Icon: SiHtml5,
     },
     {
-      text: "Next.js",
-      Icon: SiNextdotjs,
+      text: "CSS",
+      Icon: SiCss3,
     },
     {
-      text: "Tailwind",
-      Icon: SiTailwindcss,
-    },
-    {
-      text: "React",
-      Icon: SiReact,
-    },
-    {
-      text: "Next.js",
-      Icon: SiNextdotjs,
-    },
-    {
-      text: "Tailwind",
-      Icon: SiTailwindcss,
+      text: "JavaScript",
+      Icon: SiJavascript,
     },
     {
       text: "React",
@@ -43,14 +40,29 @@ export default function Skills() {
     {
       text: "Tailwind",
       Icon: SiTailwindcss,
-    }
-  ]
+    },
+    {
+      text: "Supabase",
+      Icon: SiSupabase,
+    },
+    {
+      text: "Git",
+      Icon: SiGit,
+    },
+    {
+      text: "WordPress",
+      Icon: SiWordpress,
+    },
+  ];
 
   return (
-    <div className='max-w-5xl mx-auto px-8'>
-        <Title text="Skills 🔪" className='flex flex-col items-center justify-center -rotate-6'/>
+    <div className="max-w-5xl mx-auto px-8">
+      <Title
+        text="Skills 🔪"
+        className="flex flex-col items-center justify-center -rotate-6"
+      />
 
-        <HoverEffect items={skills}/>
+      <HoverEffect items={skills} />
     </div>
-  )
+  );
 }
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,20 +9,18 @@ export default function page() {
   return (
     <div className="min-h-screen bg-black overflow-hidden">
       <div className="dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative">
-        <div className="max-w-7xl mx-auto p-5">
+        <div className="max-w-7xl mx-auto px-5 py-2 sm:py-4">
           <Navbar />
           <HeroSection />
         </div>
-        <div className="h-10 xl:h-32 bg-gradient-to-t from-black absolute -bottom-5 left-0 xl:bottom-0 w-full">
-        </div>
+        <div className="h-10 xl:h-32 bg-gradient-to-t from-black absolute -bottom-5 left-0 xl:bottom-0 w-full"></div>
       </div>
 
       <div className="max-w-7xl mx-auto p-5 mt-20">
-        <Skills/>
-        <Projects/>
-        <Footer/>
+        <Skills />
+        <Projects />
+        <Footer />
       </div>
-
     </div>
   );
 }
